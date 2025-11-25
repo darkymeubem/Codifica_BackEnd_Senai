b19e8c645182a752c55ff17dfd2b8703905f3e29
Felipe Pedroza <73255212+darkymeubem@users.noreply.github.com>
2025-11-24 21:33:52 -0300

    feat(exo-api): atividade 3

---
+++ Exo.WebApi/Program.cs
@@ -0,0 +1,23 @@
+using Exo.WebApi.Contexts;
+using Exo.WebApi.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar Entity Framework
+builder.Services.AddDbContext<ExoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<ProjetoRepository>();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
